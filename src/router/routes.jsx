93a2d50037aa0ef04c2c3100f80b4856d86e44f7
sharifulislamudoy <surifroton301@gmail.com>
2026-02-24 23:55:19 +0600

    make all the essential route protected

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -19,81 +19,146 @@ import EditJob from "../pages/EditJob";
 import Network from "../pages/Network";
 import Messages from "../pages/Messages";
 import ResumeBuilder from "../pages/ResumeBuilder";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Main,
     children: [
-        {
-            index: true,
-            Component: Home,
-        },
-        {
-          path: '/pricing',
-          Component: Payment,
-        },
-        {
-          path: '/ats-score',
-          Component: ATSScoreCheck,
-        },
-        {
-          path: '/settings',
-          Component: Settings,
-        },
-        {
-          path: '/mock-interview',
-          Component: MockInterview
-        },
-        {
-          path: '/learning-path',
-          Component: LearningPathGenerator
-        },
-        {
-          path: '/jobs',
-          Component: Jobs
-        },
-        {
-          path: '/job/:id',
-          Component: JobDetail
-        },
-        {
-          path: '/post-job',
-          Component: PostJob,
-        },
-        {
-          path: '/my-jobs',
-          Component: MyJobs,
-        },
-        {
-          path: '/my-applications',
-          Component: MyApplications,
-        },
-        {
-          path: '/apply/:id',
-          Component: JobDetail
-        },
-        {
-          path: '/job/:id/applications',
-          Component: JobApplications,
-        },
-        {
-          path: '/edit-job/:id',
-          Component: EditJob
-        },
-        {
-          path: '/network',
-          Component: Network
-        },
-        {
-          path:'/messages',
-          Component: Messages,
-        },
-        {
-          path: '/create-resume',
-          Component: ResumeBuilder,
-        }
-    ]
+      {
+        index: true,
+        Component: Home, // public home page
+      },
+      {
+        path: '/pricing',
+        element: (
+          <ProtectedRoute>
+            <Payment />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/ats-score',
+        element: (
+          <ProtectedRoute>
+            <ATSScoreCheck />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/settings',
+        element: (
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/mock-interview',
+        element: (
+          <ProtectedRoute>
+            <MockInterview />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/learning-path',
+        element: (
+          <ProtectedRoute>
+            <LearningPathGenerator />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/jobs',
+        element: (
+          <ProtectedRoute>
+            <Jobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/job/:id',
+        element: (
+          <ProtectedRoute>
+            <JobDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/post-job',
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/my-jobs',
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/my-applications',
+        element: (
+          <ProtectedRoute>
+            <MyApplications />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/apply/:id',
+        element: (
+          <ProtectedRoute>
+            <JobDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/job/:id/applications',
+        element: (
+          <ProtectedRoute>
+            <JobApplications />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/edit-job/:id',
+        element: (
+          <ProtectedRoute>
+            <EditJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/network',
+        element: (
+          <ProtectedRoute>
+            <Network />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/messages',
+        element: (
+          <ProtectedRoute>
+            <Messages />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/create-resume',
+        element: (
+          <ProtectedRoute>
+            <ResumeBuilder />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
   {
     path: '/auth',
@@ -101,12 +166,12 @@ export const router = createBrowserRouter([
     children: [
       {
         path: 'login',
-        Component: Login
+        Component: Login,
       },
       {
         path: 'sign-up',
-        Component: SignUp
-      }
-    ]
-  }
+        Component: SignUp,
+      },
+    ],
+  },
 ]);
